ff7965fb58c5b2b6dcf6941b036b0f8618add70a
Tariq5mo <tariqomer756@gmail.com>
2024-06-29 15:27:25 +0000

    Send part of task 7

--- console.py
+++ console.py
@@ -60,6 +60,26 @@ class HBNBCommand(cmd.Cmd):
             obj = models.FileStorage._FileStorage__objects[key]
             print(obj)
 
+    def do_destroy(self, line):
+        """Prints the string representation of an instance."""
+        if line == "":
+            print("** class name missing **")
+            return
+        args = line.split()
+        if args[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        if key not in models.FileStorage._FileStorage__objects:
+            print("** no instance found **")
+            return
+        else:
+            del models.FileStorage._FileStorage__objects[key]
+            models.FileStorage.save(self)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
